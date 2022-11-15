e187aa2e0f2ab595eddea3978f2a6a67ee333267
Iesha Chromie <ieshachr@va-e3.kzn.21-school.ru>
2022-11-15 21:47:22 +0300

    dich

--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Square = ({value, onClickSquare, index}) => {
+    return (
+        <button onClick ={() => onClickSquare(index)} className="square">
+          {value}
+        </button>
+    );
+}
+  
+  const Board = () => {
+    const [status, setStatus] = React.useState('X')
+    const [winner, setWinnter] = React.useState(null)
+    const [squares, setSquares] = React.useState(Array(9).fill(null))
+    const renderSquare = (i) => {
+        return <Square onClickSquare={onClickSquare} index={i} value={squares[i]}/>
+    }
+    
+    const onClickSquare = (index) => {
+      if (!winner) {
+        if (squares[index] === null) {
+          status === 'X' ? setStatus('O') : setStatus('X')
+          const arr = squares.slice()
+          arr[index] = status
+          setSquares(arr)
+          if (calculateWinner(squares)) {
+            setWinnter(calculateWinner(squares))
+          }
+        }
+      }
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return (
+    <div>
+        <div className="status">{winner ? `Winner is: ${winner}` : `Next player is: ${status}`}</div>
+        <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+        </div>
+        <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+        </div>
+        <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+        </div>
+        </div>
+      );
+}
+  
+  function Game() {
+    return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+  }
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Game />);
